cdb019328fcd527d271368f306baa76040ca686f
Cesar Kiyono <ckiyono@esss.co>
2023-06-19 08:25:24 -0300

    Components & Databinding

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() selectedRecipe = new EventEmitter<Recipe>(); 
+  recipes: Recipe[] = [
+    new Recipe('A Test Tecipe', 'This is simply a recipe', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+    new Recipe('Another Test Tecipe', 'This is simply a recipe', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe): void {
+    this.selectedRecipe.emit(recipe);
+  }
+
+}
---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,26 @@
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddItem(): void {
+    const ingrName = this.nameInputRef.nativeElement.value;
+    const ingrAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingrName, ingrAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
+
+}
