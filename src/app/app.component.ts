65ecde73fb26740a7538ae84dc2ec4887a399ab7
Cesar Kiyono <ckiyono@esss.co>
2023-11-20 10:58:16 -0300

    Add forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'First-Project';
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
 }
