684b3c2e99453c74eb52ddfac81b715a7bb70db4
Cesar Kiyono <ckiyono@esss.co>
2024-03-04 08:28:39 -0300

    Adding logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { catchError, tap } from "rxjs/operators";
 import { BehaviorSubject, throwError } from "rxjs";
 import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 export interface AuthResponseData {
     kind: string;
@@ -18,7 +19,10 @@ export interface AuthResponseData {
 export class AuthService {
     user = new BehaviorSubject<User>(null);
 
-    constructor(private http: HttpClient) {}
+    constructor(
+        private http: HttpClient,
+        private router: Router,
+    ) {}
 
     singup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
@@ -60,6 +64,11 @@ export class AuthService {
                 );
             })
         );
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
     }
 
     private handleAuthentication(email:string, userId: string, token: string, expiresIn: number) {
